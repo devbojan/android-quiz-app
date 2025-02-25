97d5c38704f30ed33fd804e5e72a4ccc818779f3
Bojan Brankovic <bojanbrankovic85@yahoo.com>
2025-02-25 19:51:11 +0100

    Quiz v0.1

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,32 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+
+            "Which company owns the android ?",
+            "Which one is not the programming language ?",
+            "Where are you watching the NBA games ?"
+
+
+    };
+
+    public static String choices[][] = {
+
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Otter","C++"},
+            {"Instagram","Facebook","What's app","Youtube"}
+
+
+    };
+
+    public static String correctAnswers[] = {
+
+            "Google",
+            "Otter",
+            "Youtube"
+
+    };
+
+
+}
